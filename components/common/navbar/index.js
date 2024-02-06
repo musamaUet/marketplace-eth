73611e0dba9560fdf0134ee10786a70b65bb9c30
musamauet <usamarabani@renesistech.com>
2024-02-06 23:55:58 +0500

    complete design work

--- components/common/navbar/index.js
+++ components/common/navbar/index.js
@@ -1,22 +1,35 @@
-export default function Footer() {
+import Link from "next/link";
 
-    return (
-      <section>
-        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-          <nav className="relative" aria-label="Global">
-            <div className="flex justify-between">
-              <div>
-                <a href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Product</a>
-                <a href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Features</a>
-                <a href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Marketplace</a>
-              </div>
-              <div>
-                <a href="#" className="font-medium mr-8 text-gray-500 hover:text-gray-900">Company</a>
-                <a href="#" className="font-medium mr-8 text-indigo-600 hover:text-indigo-500">Log in</a>
-              </div>
+export default function Footer() {
+  return (
+    <section>
+      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <nav className="relative" aria-label="Global">
+          <div className="flex justify-between">
+            <div>
+              <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                Home
+              </Link>
+              <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                Marketplace
+              </Link>
+              <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                Blogs
+              </Link>
             </div>
-          </nav>
-        </div>
-      </section>
-    )
-  }
+            <div>
+              <Link href="/" className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                Wishlist
+              </Link>
+              <a
+                href="#"
+                className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                  Connect
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
+}
---
+++ pages/courses/[slug].js
@@ -0,0 +1,46 @@
+import { Modal } from "@components/common";
+import { CourseHero, Curriculum, Keypoints } from "@components/course";
+import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/courses/fetcher";
+
+export default function Course(props) {
+  const { course } = props;
+  return (
+    <>
+      <div className="py-4">
+        <CourseHero
+          title={course.title}
+          description={course.description}
+          image={course.coverImage}
+        />
+        <Keypoints points={course.wsl}/>
+        <Curriculum locked={true} />
+        <Modal />
+      </div>
+    </>
+  );
+}
+
+export function getStaticPaths() {
+    const { data } = getAllCourses();
+
+    return {
+        paths: data.map((course) => ({
+            params: {
+                slug: course.slug
+            }
+        })),
+        fallback: false
+    }
+}
+
+export function getStaticProps({params}) {
+    const { data } = getAllCourses();
+    const course = data.filter(course=>course.slug===params.slug)[0];
+
+    return {
+        props: { course }
+    }
+}
+
+Course.Layout = BaseLayout;
